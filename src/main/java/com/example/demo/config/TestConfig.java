79fd730b4686562252e6c30987295253e3509158
unknown <fabianofac_@hotmail.com>
2021-08-22 16:06:23 -0300

    postgres configuration

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -45,33 +45,33 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Category cat1 = new Category(null, "Electronics");
+		Category cat1 = new Category(null, "Courses");
 		Category cat2 = new Category(null, "Books");
-		Category cat3 = new Category(null, "Sports");
+		Category cat3 = new Category(null, "Games");
 		
-		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
-		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
-		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
-		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		Product p1 = new Product(null, "Java, Object Oriented Programming", "Offered by University of California San Diego", 199.9, "");
+		Product p2 = new Product(null, "Call of Duty", "The stakes have never been higher as players take on the role of lethal Tier One operators in a ...", 91.9, "");
+		Product p3 = new Product(null, " Spring Boot in Action", "aCraig has a wonderful ability to explain the complex and tedious concept in simple language... ", 12.90, "");
+		Product p4 = new Product(null, "Factfulness", "Donec aliquet odio ac rhoncus cursus.", 18.90, "");
+		Product p5 = new Product(null, "The Hobbit", "Bilbo Baggins (Martin Freeman) lives a simple life with his fellow hobbits in the shire", 29.90, "");
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
-		p1.getCategories().add(cat2);
-		p2.getCategories().add(cat1);
+		p1.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
-		p3.getCategories().add(cat3);
-		p4.getCategories().add(cat3);
+		p3.getCategories().add(cat1);
+		p3.getCategories().add(cat2);
+		p4.getCategories().add(cat2);
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		User u1 = new User(null, "Mariana", "mariana@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Mario", "mariopgmail.com", "977777777", "123456"); 
+		User u2 = new User(null, "Otavio", "otaviogmail.com", "977777777", "123456"); 
 		
 		Order o1 = new Order(null, Instant.parse("2021-06-20T19:53:07Z"),OrderStatus.PAID, u1);
-		Order o2 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"),OrderStatus.CANCELED, u2);
+		Order o2 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2021-07-22T15:21:22Z"),OrderStatus.DELIVERED, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -3,7 +3,6 @@ package com.example.demo.resources;
 import java.net.URI;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
